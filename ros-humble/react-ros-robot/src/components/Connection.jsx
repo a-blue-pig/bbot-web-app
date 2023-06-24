fb7b7093708623d6146b8d8f0cb1dfdebfc5f723
Steven Ortega <stevencortega99@gmail.com>
2023-06-23 23:03:55 -0500

    Added robot connectivity checks, auto re-connect, + tested on iPad over Wifi

--- ros-humble/react-ros-robot/src/components/Connection.jsx
+++ ros-humble/react-ros-robot/src/components/Connection.jsx
@@ -1,6 +1,52 @@
+import { useState } from "react";
+import Alert from "react-bootstrap/Alert"
+import Config from "../scripts/Config"
+
 function Connection() {
+    const [connected, isConnected] = useState(false);
+    const ros = new window.ROSLIB.Ros({ encoding: 'ascii' })
+
+    function initConnection() {
+        ros.connect("ws://" + Config.ROSBRIDGE_SERVER_IP + ":" + Config.ROSBRIDGE_SERVER_PORT);
+    }
+    function connect() {
+        try {
+            initConnection();
+        } catch (error) {
+            console.log('error initiating connection');
+        }
+        // won't let the user connect more than once
+        ros.on('error', function (error) {
+            console.log(error);
+            isConnected(false);
+        });
+
+        ros.on('connection', function () {
+            console.log('Connected!');
+            isConnected(true);
+        });
+
+        ros.on('close', function () {
+            console.log('Connection closed');
+            isConnected(false);
+            setTimeout(() => {
+                try {
+                    initConnection();
+                } catch (error) {
+                    console.log('error initiating connection');
+                }
+
+            }, 1000);
+            
+        });
+    }
+
+    window.onload = connect();
+
     return (
-        <h2>Connection</h2>
+        <div>
+            <Alert className="text-center m-3" variant={connected ? 'success' : 'danger'}> {connected ? 'Robot Connected' : 'Robot Disconnected'}</Alert>
+        </div>
     );
 }
 
---
+++ ros-humble/react-ros-robot/src/scripts/Config.js
@@ -0,0 +1,6 @@
+const Config = {
+    ROSBRIDGE_SERVER_IP: "10.149.11.97",
+    ROSBRIDGE_SERVER_PORT: "9090",
+};
+
+export default Config;
